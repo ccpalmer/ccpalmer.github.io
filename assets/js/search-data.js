8b51a09f5e67b09e1eb0576e8ba013786a0c5a56
ccpalmer <ccpalmer@dartmouth.edu>
2025-06-27 20:46:14 +0000

    Deploying to gh-pages from @ ccpalmer/ccpalmer.github.io@52c082a6a3b025333691e2524657cb0b0bd6bc6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,16 +418,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-first-day-of-fall-2025-classes-tada",
+          title: 'First day of Fall 2025 classes ! :tada:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
