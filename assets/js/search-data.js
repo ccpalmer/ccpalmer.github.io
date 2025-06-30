dae3a02183e276d696df9b496204e3893606cabb
ccpalmer <ccpalmer@dartmouth.edu>
2025-06-30 19:13:14 +0000

    Deploying to gh-pages from @ ccpalmer/ccpalmer.github.io@9f5f557ae3e17d796cf484a6ef1c8a210a014ec8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,65 +72,30 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-formatting-and-links",
+            },{id: "post-a-new-location-a-new-university-a-new-course",
         
-          title: "a post with formatting and links",
+          title: "A new location, a new university, a new course!",
         
-        description: "march &amp; april, looking forward to summer",
+        description: "Looking forward to Fall !",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/formatting-and-links/";
+            window.location.href = "/blog/2025/Hello/";
           
         },
       },{id: "news-first-day-of-fall-2025-classes-tada",
           title: 'First day of Fall 2025 classes ! :tada:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
-          description: "a project with a background image",
+          description: "Punkin Palmer Memorial Observatory",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/2_project/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
